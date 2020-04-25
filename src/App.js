0e9c4e664b51c8cda4917649fc2388f67f8508a4
gee <special.geeee@gmail.com>
2020-04-25 03:18:08 +0200

    replaced carousel implementation with Slider component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import { authenticationService } from './services';
 import { LoginPage } from './pages';
 import { PrivateRoute } from './components/PrivateRoute';
 import Admin from './components/Admin';
+import Slider from './components/Slider';
 import VideoPlayer from './components/VideoPlayer';
 import Footer from './components/Footer';
 import './App.css';
 import  infoIcon from './assets/img/info-icon.png';
-import deuspiCover from './assets/img/deuspi-cover.png';
-import categoryCover from './assets/img/category-cover.png';
-import Swiper from 'react-id-swiper';
-import 'swiper/css/swiper.css';
 
 let data = [];
 
@@ -48,128 +45,6 @@ function parseItemsByCategory(_data, item) {
   })
 }
 
-function setSlidesTransform() {
-  let slides = document.getElementsByClassName("swiper-slide");
-  
-  [].forEach.call(slides, slide => {
-
-    slide.onmouseover = () => {
-      let translateZ = getComputedTranslateZ(slide);
-
-      slide.style.transform = "translate3d(0px, 0px, " + String(translateZ) +  "px) rotateX(0deg) rotateY(0deg) scale(1.1)";
-      slide.style.transitionDuration = "300ms";
-    }
-
-    slide.onmouseout = () => {
-      let translateZ = getComputedTranslateZ(slide);
-      
-      slide.style.transform = "translate3d(0px, 0px, " + String(translateZ) +  "px) rotateX(0deg) rotateY(0deg) scale(1)";
-    }
-  });
-}
-
-function getComputedTranslateZ(el) {
-    if(!window.getComputedStyle) return;
-    
-    let style = getComputedStyle(el);
-    let transform = style.transform || style.webkitTransform || style.mozTransform;
-    let mat = transform.match(/^matrix3d\((.+)\)$/);
-
-    return mat ? ~~(mat[1].split(', ')[14]) : 0;
-}
-
-function getSlideClickEvent() {
-  let swiperWrapper = document.querySelector('.swiper-wrapper');
-  swiperWrapper.addEventListener('click', function(event) {
-      if (event.target.tagName === 'IMG') {
-        let slideElement = event.target;
-        let playlist = slideElement.getAttribute("data-playlist");
-        console.log(playlist);
-      
-        new Promise((resolve, reject) => {
-          resolve();
-        }).then(() => {
-            
-        }).then(() => {
-            
-        }).then(() => {
-            
-        }).catch(() => {
-      
-        }).finally(() => {
-      
-        });      
-      }
-  });  
-}
-
-const MutipleSlidesPerView = () => {
-  const params = {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    speed: 300,
-    centeredSlides: true,
-    prevenClicks: true,
-    preventClicksPropagation: true,
-    slideToClickedSlide: true,
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    //   dynamicBullets: true
-    // },
-    effect: 'coverflow',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true
-    },
-    zoom: {
-      maxRatio: 1.1,
-      toggle: false
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 40
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 10
-      }
-    }
-  }
-
-  return (
-    <Swiper {...params}>
-      <div><img className="swiper-slide-image" data-playlist="deuspi" src={deuspiCover} alt="deuspi cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="aaaaaaa" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="bbbbbbb" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="ccccccc" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="ddddddd" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="eeeeeee" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="fffffff" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="ggggggg" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="hhhhhhh" src={categoryCover} alt="category cover" /></div>
-      <div><img className="swiper-slide-image" data-playlist="iiiiiii" src={categoryCover} alt="category cover" /></div>
-    </Swiper>
-  )
-};
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -184,11 +59,6 @@ class App extends React.Component {
 
   componentDidMount() {
     parseData(this.props.appData);
-
-    setSlidesTransform();
-
-    // fix click on duplicate slides
-    getSlideClickEvent();
 
     authenticationService.currentUser.subscribe(x => this.setState({
       currentUser: x,
@@ -216,9 +86,11 @@ class App extends React.Component {
                 <div className="row">
                   <div className="col-lg">
                     <div className="align-items-center justify-content-center">
-                    {/* <VideoPlayer data={data} ref={this.videoPlayerRef} /> */}
-                    {/* <button onClick={() => this.videoPlayerRef.current.onUpdateVideo()}>RANDOM</button> */}
-                    <Footer />
+                      <Slider />
+                      <VideoPlayer data={data} ref={this.videoPlayerRef} />
+                      {/* <button onClick={() => this.videoPlayerRef.current.onUpdateVideo()}>RANDOM</button> */}
+                      <Footer />
+                    </div>
                   </div>
                 </div>
               </div>    
