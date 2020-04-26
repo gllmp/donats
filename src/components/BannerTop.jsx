2af73587180a87310048c49ef0a13f5d34c4503c
gee <special.geeee@gmail.com>
2020-04-26 16:18:50 +0200

    handle swap on close icon click in BannerTop component

--- src/components/BannerTop.jsx
+++ src/components/BannerTop.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import  infoIcon from '../assets/img/info-icon.png';
+import  closeIcon from '../assets/img/close-icon.png';
 
 class BannerTop extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onClose = this.onClose.bind(this);
+    }
+
+    componentDidMount() {
+    
+    }
+
+    onClose() {
+        this.props.handleSwap("close");
+    }
+
     render() {
         return (
             <nav id="banner-top" className="navbar navbar-expand-lg">
-              <a id="link-title" className="navbar-nav mr-auto navbar-brand" href="/">DONATS</a>
-              <div id="link-info" className="navbar-nav ml-auto"><img src={infoIcon} alt="info icon" /></div>
+              <a id="banner-title" className="navbar-nav mr-auto navbar-brand" href="/">DONATS</a>
+              <div id="banner-info" className="navbar-nav ml-auto"><img src={infoIcon} alt="info icon" /></div>
+              <div id="banner-close" className="navbar-nav ml-auto" onClick={this.onClose}><img src={closeIcon} alt="close icon" /></div>
             </nav>
         )
     }
