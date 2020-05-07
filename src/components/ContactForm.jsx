d046385c23bf4069d65feecbeeeae656618183af
gee <special.geeee@gmail.com>
2020-05-07 15:17:19 +0200

    remove ContactForm unused code

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,13 +5,6 @@ class ContactForm extends React.Component {
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     user_email: "",
-        //     contact_number: "",
-        //     user_name: "",
-        //     message: ""
-        // };
-
         this.sendEmail = this.sendEmail.bind(this);
     }
 
@@ -21,8 +14,6 @@ class ContactForm extends React.Component {
 
     sendEmail(e) {
         e.preventDefault();
-
-        //console.log("EMAIL: ", this.state);
         
         emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICEID, process.env.REACT_APP_EMAILJS_TEMPLATEID, e.target, process.env.REACT_APP_EMAILJS_USERID)
         .then((result) => {
@@ -38,13 +29,10 @@ class ContactForm extends React.Component {
             <input type="hidden" name="contact_number" />
             <label>Name</label>
             <input type="text" name="user_name" />
-            {/* <input type="text" name="user_name" onChange={e => this.setState({user_name: e.target.value})} /> */}
             <label>Email</label>
             <input type="email" name="user_email" />
-            {/* <input type="email" name="user_email" onChange={e => this.setState({user_email: e.target.value})} /> */}
             <label>Message</label>
             <textarea name="message" />
-            {/* <textarea name="message" onChange={e => this.setState({message: e.target.value})} /> */}
             <input type="submit" value="Send" />
             </form>
         );    
