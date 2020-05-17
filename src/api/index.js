20cd89804d039f9d2af329a6c12af1fbea160f8f
gee <special.geeee@gmail.com>
2020-05-18 01:06:20 +0200

    added category api

--- src/api/index.js
+++ src/api/index.js
@@ -10,12 +10,23 @@ export const updateVideoById = (id, payload) => api.put(`/video/${id}`, payload)
 export const deleteVideoById = id => api.delete(`/video/${id}`);
 export const getVideoById = id => api.get(`/video/${id}`);
 
+export const insertCategory = payload => api.post(`/category`, payload);
+export const getAllCategories = () => api.get(`/categories`);
+export const updateCategoryById = (id, payload) => api.put(`/category/${id}`, payload);
+export const deleteCategoryById = id => api.delete(`/category/${id}`);
+export const getCategoryById = id => api.get(`/category/${id}`);
+
 const apis = {
     insertVideo,
     getAllVideos,
     updateVideoById,
     deleteVideoById,
     getVideoById,
+    insertCategory,
+    getAllCategories,
+    updateCategoryById,
+    deleteCategoryById,
+    getCategoryById,
 }
 
 export default apis;
