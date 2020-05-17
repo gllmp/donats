db5be0ddcb87c153dc1268fe53a19d0c86323115
gee <special.geeee@gmail.com>
2020-05-18 01:06:55 +0200

    added CategoryInsert component

---
+++ src/pages/CategoryInsert.jsx
@@ -0,0 +1,193 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../api';
+//import FileUpload from '../components/FileUpload'
+
+//import data from '../data.json'
+
+class CategoryInsert extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            category: [],
+            cover: '',
+            url: ''
+        }
+
+        this.handleChangeInputName = this.handleChangeInputName.bind(this);
+        this.handleChangeInputCategory = this.handleChangeInputCategory.bind(this);
+        this.handleChangeInputCover = this.handleChangeInputCover.bind(this);
+        this.handleChangeInputUrl = this.handleChangeInputUrl.bind(this);
+    }
+
+    handleChangeInputName = async event => {
+        const name = event.target.value;
+        this.setState({ name });
+    }
+
+    handleChangeInputCategory = async event => {
+        const category = event.target.value;
+        this.setState({ category });
+    }
+
+    handleChangeInputCover = async event => {
+        const cover = event.target.value;
+        this.setState({ cover });
+    }
+
+    handleChangeInputUrl = async event => {
+        const url = event.target.value;
+        this.setState({ url });
+    }    
+
+    // handleIncludeDataFromFile = async () => {
+    //      // /!\ REMEMBER TO ADD [] AT START AND END OF JSON FILE
+    //     const _this = this;
+
+    //     console.log("FILE: ", data);
+
+    //     data.map( (data) => {        
+    //         let skate = data.category.skate;
+        
+    //         skate.forEach(element => {
+    //             console.log(element);
+
+    //             _this.state.url = element.id;
+    //             _this.state.category = "SKATE";
+
+    //             const { title, url, category } = _this.state;
+
+    //             let payload = { title, url, category };
+                
+    //             new Promise((resolve, reject) => {
+    //                 console.log(_this.state)
+    //                 getYoutubeTitle(_this.state.url, API_KEY, function (err, title) {
+    //                     console.log("TITLE: ", title);
+    //                     if (err) console.log("ERROR: ", err);
+        
+    //                     _this.setState({ title });
+    //                     payload.title = title;
+                        
+    //                     resolve();
+    //                 })    
+    //             }).then(() => {
+    //                 api.insertVideo(payload)
+    //                 .then(res => {
+    //                     console.log('Video inserted successfully');
+    //                     this.setState({
+    //                         title: '',
+    //                         url: '',
+    //                         category: '',
+    //                     })
+    //                 }).finally(() => {
+    //                     //window.location.href = `/admin`;
+    //                 });
+        
+    //             }).catch((error) => {
+    //                  console.error(error);
+    //             }).finally(() => {
+            
+    //             });    
+
+                
+    //         });
+    //     })
+    // }
+
+    handleIncludeCategory = async () => {
+        const { name, category, cover, url } = this.state;
+        
+        if (!name) {
+            alert("Please add name");
+        } else {
+            let payload = { name, category, cover, url };
+        
+            new Promise((resolve, reject) => {
+                resolve();
+            }).then(() => {
+
+            }).then(() => {
+                // ADD NAME TO CATEGORY ARRAY
+                let categoryArray = [];
+                categoryArray.push(name);
+
+                //let categoryArrayDuplicate = [...this.state.category];
+                //categoryArrayDuplicate.push(name);
+
+                this.setState({ 
+                    category: categoryArray
+                })
+        
+                console.log("CATEGORY STATE: ", this.state);
+            }).then(() => {
+                payload.name = name;
+                payload.category = category;
+                payload.cover = cover;
+                payload.url = url;
+            }).then(() => {
+                api.insertCategory(payload)
+                .then(res => {
+                    window.alert(`Video inserted successfully`);
+
+                    this.setState({
+                        name: '',
+                        category: '',
+                        cover: '',
+                        url: ''
+                    })
+                });
+            }).catch((error) => {
+                console.error(error);
+            }).finally(() => {
+        
+            });    
+        }        
+    }
+
+    render() {
+        const { name, category, cover, url } = this.state;
+
+        return (
+            <div id="category-insert-container">
+                {/* <FileUpload /> */}
+                
+                <h1 id="category-insert-title">CATEGORY MANAGER</h1>
+
+                <div id="category-insert-name">
+                    <label>NOM: </label>
+                    <input id="category-name-input" className="form-control" type="text" value={name} onChange={this.handleChangeInputName} />
+                </div>
+
+                {/* <label className="mt-4">CATEGORY: </label>
+                <select className="form-control" type="text" value={category} onChange={this.handleChangeInputCategory}>
+                    <option value="" hidden></option>
+                    <option value="MUSIC">MUSIC</option>
+                    <option value="RAP">RAP FR</option>
+                    <option value="SKATE">SKATE</option>
+                </select>
+                <ul>
+                    {this.state.category.map(item => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ul>
+
+
+                <label>COVER: </label>
+                <input className="form-control" type="text" value={cover} onChange={this.handleChangeInputCover} />
+
+
+                <label>URL: </label>
+                <input className="form-control" type="text" value={url} onChange={this.handleChangeInputUrl} /> */}
+
+                <div id="category-insert-button-container">
+                    <button className="btn btn-primary" onClick={this.handleIncludeCategory}>SAVE</button>
+                    <Link to="/admin/categories/list" className="btn btn-danger">CANCEL</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CategoryInsert;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import AdminPage from './AdminPage';
 import VideoList from './VideoList';
 import VideoInsert from './VideoInsert';
 import VideoUpdate from './VideoUpdate';
+import CategoryInsert from './CategoryInsert';
 
-export { LoginPage, AdminPage, VideoList, VideoInsert, VideoUpdate };
+export { LoginPage, AdminPage, VideoList, VideoInsert, VideoUpdate, CategoryInsert };
