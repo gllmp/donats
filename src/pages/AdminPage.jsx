1698020dbbe1cbdf5fa2f85791db529e7c068b38
gee <special.geeee@gmail.com>
2020-06-01 02:05:50 +0200

    added link to homepage from admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 function AdminPage() {
     return (
         <div id="admin-title-container">
-            <p id="admin-title-main">DONATS</p>
-            <p id="admin-title-sub">Le hasard fait bien les choses</p>
+            <a href="/">
+                <p id="admin-title-main">DONATS</p>
+                <p id="admin-title-sub">Le hasard fait bien les choses</p>
+            </a>
         </div>
     )
 }
