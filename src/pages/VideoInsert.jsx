f729582b7861b2e25c7cd5105b4124dfef84779d
gee <special.geeee@gmail.com>
2020-06-25 03:31:01 +0200

    enable bulk import of videos by id from JSON file

--- src/pages/VideoInsert.jsx
+++ src/pages/VideoInsert.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import getYoutubeTitle from 'get-youtube-title';
 import styled from 'styled-components';
 import api from '../api';
-//import FileUpload from '../components/FileUpload'
+import DragAndDropVideoFile from '../components/DragAndDropVideoFile'
+// import FileUpload from '../components/FileUpload'
 
 //import data from '../data.json'
 
@@ -55,7 +56,8 @@ class VideoInsert extends Component {
     }
 
     componentDidMount = async () => {
-        
+        this.dragAndDropRef = React.createRef();
+
         await api.getAllCategories().then(categories => {
             this.setState({
                 savedCategories: categories.data.data,
@@ -68,6 +70,7 @@ class VideoInsert extends Component {
             const optionsList = result.map((category) =>
                 <option value={category.name} key={category.name.toString()}>{category.name}</option>
             );
+
 
             this.setState({
                 categoriesSelectItems: optionsList,
@@ -99,70 +102,85 @@ class VideoInsert extends Component {
         this.setState({ category });
     }
 
-    // handleIncludeDataFromFile = async () => {
-    //      // /!\ REMEMBER TO ADD [] AT START AND END OF JSON FILE
-    //     const _this = this;
-
-    //     console.log("FILE: ", data);
-
-    //     data.map( (data) => {        
-    //         let skate = data.category.skate;
+    handleIncludeDataFromFile = async () => {
+         // /!\ REMEMBER TO ADD [] AT START AND END OF JSON FILE
+        const _this = this;
         
-    //         skate.forEach(element => {
-    //             console.log(element);
+        const data = this.dragAndDropRef.current.state.data;
+        console.log("FILE DATA: ", data[0]);
 
-    //             _this.state.url = element.id;
-    //             _this.state.category = "SKATE";
+        let currentCategory;
+        let currentCategoryIndex = 0;
 
-    //             const { title, url, category } = _this.state;
+        if (data.length > 0) {
+            data.map( (data) => {      
+                Object.keys(data.category).forEach(category => {
+                    currentCategory = category;
 
-    //             let payload = { title, url, category };
-                
-    //             new Promise((resolve, reject) => {
-    //                 console.log(_this.state)
-    //                 getYoutubeTitle(_this.state.url, API_KEY, function (err, title) {
-    //                     console.log("TITLE: ", title);
-    //                     if (err) console.log("ERROR: ", err);
-        
-    //                     _this.setState({ title });
-    //                     payload.title = title;
+                    data.category[category].forEach(element => {
+                        _this.state.url = element.id;
+                        _this.state.category = currentCategory.toUpperCase();
+
+                        const { title, url, category } = _this.state;
+
+                        let payload = { title, url, category };
                         
-    //                     resolve();
-    //                 })    
-    //             }).then(() => {
-    //                 api.insertVideo(payload)
-    //                 .then(res => {
-    //                     console.log('Video inserted successfully');
-    //                     this.setState({
-    //                         title: '',
-    //                         url: '',
-    //                         category: '',
-    //                     })
-    //                 }).finally(() => {
-    //                     //window.location.href = `/admin`;
-    //                 });
-        
-    //             }).catch((error) => {
-    //                  console.error(error);
-    //             }).finally(() => {
-            
-    //             });    
-
+                        new Promise((resolve, reject) => {
+                            //console.log(_this.state)
+                            getYoutubeTitle(_this.state.url, API_KEY, function (err, title) {
+                                console.log("TITLE: ", title);
+                                if (err) console.log("ERROR: ", err);
                 
-    //         });
-    //     })
-    // }
+                                _this.setState({ title });
+                                payload.title = title;
+                                
+                                resolve();
+                            })    
+                        }).then(() => {
+                            api.insertVideo(payload)
+                            .then(res => {
+                                this.setState({
+                                    title: '',
+                                    url: '',
+                                    category: '',
+                                })
+                            }).finally(() => {
+                                //window.location.href = `/admin`;
+                            });
+                
+                        }).catch((error) => {
+                            console.error(error);
+                        }).finally(() => {
+                            currentCategoryIndex++;
+                            if (currentCategoryIndex === data.category[currentCategory].length) {
+                                window.alert(`Vidéo ajoutée avec succès`);
+                            }
+                        });
+                    });
+                });
+            })
+        } else {
+            alert("Veuillez ajouter un fichier ou remplir les champs avant de continuer");
+        }
+
+    }
 
     handleIncludeVideo = async () => {
         const _this = this;
+        
+        const data = this.dragAndDropRef.current.state.data;    
 
         const { title, url, category } = this.state;
-        
-        if (!url) {
+
+        if (!url && data.length === 0) {
             alert("Ajoutez une URL avant de continer");
-        } else if (!category) {
+        } else if (!category && data.length === 0) {
             alert("Ajoutez une catégorie avant de continuer");
+        } else if (data.length > 0) {
+            // Upload data from file
+            this.handleIncludeDataFromFile();
         } else {
+            // Upload data from form
             let payload = { title, url, category };
         
             await new Promise((resolve, reject) => {
@@ -178,7 +196,6 @@ class VideoInsert extends Component {
             }).then(() => {
                 api.insertVideo(payload)
                 .then(res => {
-                    window.alert(`Vidéo ajoutée avec succès`);
                     this.setState({
                         title: '',
                         url: '',
@@ -186,12 +203,11 @@ class VideoInsert extends Component {
                     })
                 }).finally(() => {
                     //window.location.href = `/admin`;
-                });
-    
+                });    
             }).catch((error) => {
                 console.error(error);
             }).finally(() => {
-        
+                window.alert(`Vidéo ajoutée avec succès`);
             });
         }        
     }
@@ -201,10 +217,11 @@ class VideoInsert extends Component {
         return (
             <div id="video-insert-container">
                 {!this.state.isLoading && (
-                    <Wrapper className="col-sm-4 offset-md-4">
-                        {/* <FileUpload /> */}
-                        
+                    <Wrapper className="col-sm-4 offset-md-4">                        
                         <Title className="mb-4">ADD VIDEO</Title>
+
+                        <DragAndDropVideoFile ref={this.dragAndDropRef} />
+                        {/* <FileUpload /> */}
 
                         <Label>URL: </Label>
                         <InputText
