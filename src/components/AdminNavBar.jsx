2ba13d0ec643ca297ff08d2cb8150b08f0106099
gcode <gcode.wav@gmail.com>
2021-10-19 16:58:05 +0200

    fixed admin navbar style

--- src/components/AdminNavBar.jsx
+++ src/components/AdminNavBar.jsx
@@ -5,7 +5,7 @@ import { Navbar } from 'react-bootstrap';
 class AdminNavBar extends Component {
     render() {
         return (
-            <Navbar className="admin-navbar-container" expand="lg">
+            <Navbar className="admin-navbar-container navbar-dark" expand="lg">
             <Navbar.Brand id="admin-navbar-title" className="ml-4" href={process.env.PUBLIC_URL + "/admin"}><p>DONATS</p>ADMIN</Navbar.Brand>
             <Navbar.Toggle aria-controls="admin-navbar-nav" />
             <Navbar.Collapse id="admin-navbar-nav">
