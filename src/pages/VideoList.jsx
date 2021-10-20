36ebdae7ae425af22c6c365795f2bcba7f1cd90f
gcode <gcode.wav@gmail.com>
2021-10-20 21:58:24 +0200

    delete multiple videos by id using checkboxes #22

--- src/pages/VideoList.jsx
+++ src/pages/VideoList.jsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 
 import '../assets/styles/react-table-inverted.css';
 import { Checkbox } from '@material-ui/core';
+import loadingCircle from '../assets/img/loading-circle.gif'
 
 const Wrapper = styled.div`
 
@@ -27,8 +28,13 @@ class DeleteVideo extends Component {
             )
         ) {
             await new Promise(async (resolve, reject) => {
-                await api.deleteVideoById(this.props.id).then( () => {
+                await api.deleteVideoById(this.props.id)
+                .then( () => {
                     resolve();
+                }).catch((error) => {
+                    console.error(error);
+                }).finally(() => {
+
                 });
             }).then(async () => {
                 await alert("Vidéo supprimée avec succès !");
@@ -53,6 +59,7 @@ class VideoList extends Component {
             videos: [],
             columns: [],
             isLoading: true,
+            isDeleting: false
         }
 
         this.reactTableRef = React.createRef();
@@ -60,6 +67,8 @@ class VideoList extends Component {
 
         this.onPageChange = this.onPageChange.bind(this);
         this.onSelectAllPageCheckboxes = this.onSelectAllPageCheckboxes.bind(this);
+        this.onDeleteSelectedItems = this.onDeleteSelectedItems.bind(this);
+        this.createHeaderIcons = this.createHeaderIcons.bind(this);
     }
 
     onPageChange(page) {
@@ -101,6 +110,118 @@ class VideoList extends Component {
         // console.log(linkCheckboxes.length)
     }
 
+    onDeleteSelectedItems = async (event) => {
+        // let selectCheckbox = document.getElementsByClassName("select-checkbox")[0];
+        let linkCheckboxes = document.getElementsByClassName("link-checkbox");
+        let selectedCheckboxes = [];
+        let videoIds = [];
+        let currentIdIndex = 0;
+
+        for (let i=0; i<linkCheckboxes.length; i++) {
+            if (linkCheckboxes[i].checked === true) {
+                selectedCheckboxes.push(linkCheckboxes[i]);
+            }
+        }
+
+        if (selectedCheckboxes.length) {
+            if (
+                window.confirm(
+                    "Voulez-vous supprimer la sélection définitivement ?",
+                )
+            ) {
+                this.setState({
+                    isDeleting: true
+                })
+
+                await selectedCheckboxes.map(async (video) => {
+                    videoIds.push(video.id);
+                });
+                
+                Object.values(videoIds).forEach(async (id) => {
+                    await new Promise(async (resolve, reject) => {
+                        resolve(id);
+                    }).then(async (id) => {
+                        await api.deleteVideoById(id).then(async () => {
+                            console.log("Vidéo: " + id + " supprimée avec succès !");
+                        }).catch((error) => {
+                            console.error(error);
+                        }).finally(() => {
+
+                        });
+                    }).then(async () => {
+                        
+                    }).catch((error) => {
+                        console.error(error);
+
+                        this.setState({
+                            isDeleting: false
+                        })
+                    }).finally(() => {
+                        currentIdIndex++;
+
+                        if (currentIdIndex === videoIds.length) {
+                            this.setState({
+                                isDeleting: false
+                            })
+
+                            alert("Sélection supprimée avec succès !")
+                            window.location.reload(true);
+                        }
+                    });
+
+                })
+            }
+        } else {
+            alert("Aucun élément sélectionné");
+        }
+    }
+
+    createHeaderIcons() {
+        // Select Checkbox
+        let selectHeaderElement = document.getElementsByClassName("rt-thead")[1].getElementsByClassName("rt-tr")[0].lastChild;
+        selectHeaderElement.className = "select-header";
+
+        let selectInputElement = document.createElement("input");
+        selectInputElement.type = "checkbox";
+        selectInputElement.className = "select-checkbox";
+        selectInputElement.name = "select-checkbox";
+
+        selectInputElement.onclick = (event) => {
+            this.onSelectAllPageCheckboxes(event)
+        }
+
+        selectHeaderElement.appendChild(selectInputElement);
+
+        // Bin Icon
+        // let binSvgElement = document.createElement("svg");
+        // binSvgElement.xmlns = "http://www.w3.org/2000/svg";
+        // binSvgElement.width = 24;
+        // binSvgElement.height = 24;
+        // binSvgElement.viewBox = "0 0 24 24";
+        // binSvgElement.className = "bin-icon";
+
+        // let binPathElement = document.createElement("path");
+        // binPathElement.d = "M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z";
+
+        // binSvgElement.appendChild(binPathElement);
+
+        // binSvgElement.onclick = (event) => {
+        //     this.onDeleteSelectedItems(event)
+        // }
+
+        // console.log(binSvgElement)
+
+        let domParser = new DOMParser();
+        let svgString='<svg xmlns="http://www.w3.org/2000/svg" class="bin-icon" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>';
+        let svgElement = domParser.parseFromString(svgString, "image/svg+xml");
+
+        svgElement.documentElement.onclick = (event) => {
+            this.onDeleteSelectedItems(event)
+        }
+
+        selectHeaderElement.appendChild(svgElement.documentElement);
+    }
+
     componentDidMount = async () => {
         
         await api.getAllVideos().then(videos => {
@@ -109,20 +230,8 @@ class VideoList extends Component {
                 isLoading: false,
             })
         }).then(() => {
-            let selectHeaderElement = document.getElementsByClassName("rt-thead")[1].getElementsByClassName("rt-tr")[0].lastChild;
-            selectHeaderElement.className = "select-header";
+            this.createHeaderIcons();
 
-            let selectInputElement = document.createElement("input");
-            selectInputElement.type = "checkbox";
-            selectInputElement.className = "select-checkbox";
-            selectInputElement.name = "select-checkbox";
-
-            selectInputElement.onclick = (event) => {
-                this.onSelectAllPageCheckboxes(event)
-            }
-
-            selectHeaderElement.appendChild(selectInputElement);
-            
         }).catch((error) => {
             console.error(error);
         }).finally(() => {
@@ -131,7 +240,7 @@ class VideoList extends Component {
     }
 
     render() {
-        const { videos, isLoading } = this.state;
+        const { videos, isLoading, isDeleting } = this.state;
 
         const columns = [
             // {
@@ -201,6 +310,9 @@ class VideoList extends Component {
 
         return (
             <Wrapper id="admin-list-container" className="col-sm-8 offset-sm-2">
+                {this.state.isDeleting &&
+                    <img className="loading-circle" alt="gif" src={loadingCircle} />
+                }
                 {showTable && (
                     <ReactTable
                         data={videos}
